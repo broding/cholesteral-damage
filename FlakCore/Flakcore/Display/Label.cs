4472209c539941851495367d3648f500195424c6
Bas Roding <bas.roding@gmail.com>
2013-01-22 23:12:44 +0100

    Added label

---
+++ FlakCore/Flakcore/Display/Label.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework;
+
+namespace Flakcore.Display
+{
+    public class Label : Sprite
+    {
+        public string Text;
+        public SpriteFont SpriteFont;
+        public HorizontalAlign HorizontalAlign;
+        public VerticalAlign VerticalAlign;
+
+        private Vector2 TextSize;
+
+        public Label() : this("", GameManager.Content.Load<SpriteFont>("DefaultFont"))
+        { 
+        }
+
+        public Label(string text) : this(text, GameManager.Content.Load<SpriteFont>("DefaultFont"))
+        {
+        }
+
+        public Label(string text, SpriteFont spriteFont)
+        {
+            this.Text = text;
+            this.SpriteFont = spriteFont;
+            this.SpriteFont = GameManager.Content.Load<SpriteFont>("DefaultFont");
+            this.HorizontalAlign = HorizontalAlign.LEFT;
+            this.VerticalAlign = VerticalAlign.TOP;
+
+            this.TextSize = this.SpriteFont.MeasureString(text);
+            this.Width = (int)this.TextSize.X;
+            this.Height = (int)this.TextSize.Y;
+        }
+
+
+        protected override void DrawCall(SpriteBatch spriteBatch, ParentNode parentNode)
+        {
+            if (this.Text == "")
+                return;
+
+            this.TextSize = this.SpriteFont.MeasureString(this.Text);
+
+            parentNode.Position.X *= this.ScrollFactor.X;
+            parentNode.Position.Y *= this.ScrollFactor.Y;
+            parentNode.Position.X += this.Width / 2 - this.TextSize.X / 2;
+            parentNode.Position.Y += this.Height / 2 - this.TextSize.Y / 2;
+
+            spriteBatch.DrawString(
+                GameManager.FontDefault,
+                this.Text,
+                parentNode.Position,
+                this.Color * parentNode.Alpha,
+                this.Rotation,
+                this.Origin,
+                this.Scale,
+                this.SpriteEffects,
+                Node.GetDrawDepth(this.GetParentDepth()));
+        }
+    }
+
+    public enum HorizontalAlign
+    {
+        LEFT,
+        CENTER,
+        RIGHT
+    }
+
+    public enum VerticalAlign
+    {
+        TOP,
+        MIDDLE,
+        BOTTOM
+    }
+}
