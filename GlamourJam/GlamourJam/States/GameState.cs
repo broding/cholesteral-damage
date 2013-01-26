b220ce67973378762396eb5881dbd02b2f68d7be
broding <bas.roding@hva.nl>
2013-01-26 12:55:55 +0100

    added yellowblur to gamestate

--- GlamourJam/GlamourJam/States/GameState.cs
+++ GlamourJam/GlamourJam/States/GameState.cs
@@ -6,6 +6,7 @@ using Flakcore.Display;
 using Display.Tilemap;
 using Microsoft.Xna.Framework;
 using Flakcore;
+using Flakcore.Display.ParticleEngine;
 
 namespace GlamourJam.States
 {
@@ -19,6 +20,8 @@ namespace GlamourJam.States
         public Vetbol player3;
 
         public Vetbol player4;
+        ParticleEngine particle;
+
 
         public GameState()
         {
@@ -29,20 +32,43 @@ namespace GlamourJam.States
             this.AddChild(bg);
 			this.AddChild(tilemap);
 
-
             CapturePoint capturepoint = new CapturePoint();
             AddChild(capturepoint);
 
-			
 			player = new Vetbol(PlayerIndex.One);
             player2 = new Vetbol(PlayerIndex.Two);
 			AddChild(player);
             AddChild(player2);
+
+            particle = new ParticleEngine(Controller.Content.Load<ParticleEffect>("splashBottom"));
+
+            this.AddChild(particle);
+            particle.Position = new Vector2(400, 400);
+
+            YellowBlur blur = new YellowBlur();
+            this.AddChild(blur);
+
+            this.ConvertBloodTiles();
         }
 		public override void Update(Microsoft.Xna.Framework.GameTime gameTime)
 		{
-
+            if(Controller.Input.JustPressed(PlayerIndex.One, Microsoft.Xna.Framework.Input.Keys.Space))
+            {
+                particle.Start(300);
+            }
 			base.Update(gameTime);
 		}
+
+        public void ConvertBloodTiles()
+        {
+            List<Tile> tiles = this.tilemap.RemoveTiles(7);
+
+            foreach (Tile tile in tiles)
+            {
+                ParticleEngine engine = new ParticleEngine(Controller.Content.Load<ParticleEffect>("bloodParticle"));
+                engine.Position = tile.Position + new Vector2(0, 16);
+                this.AddChild(engine);
+            }
+        }
     }
 }
