9494ddc807d2a811590fcee41372c7ce03359e50
SergeantGarden <hugomater@hotmail.com>
2013-01-26 12:19:07 +0100

    Adds a blur when an instance of YellowBlur is made, it can get more visible calling next

---
+++ GlamourJam/GlamourJam/YellowBlur.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Flakcore.Display;
+using Flakcore;
+using Microsoft.Xna.Framework.Input;
+using Microsoft.Xna.Framework;
+
+namespace GlamourJam
+{
+    class YellowBlur : Sprite
+    {
+        public YellowBlur()
+        {
+            this.LoadTexture(@"YellowBlur");
+            this.AlphaState = AlphaStates.NotVisible;
+        }
+
+        public enum AlphaStates
+        {
+            NotVisible = 0,
+
+            FirstPointCaptured = 2,
+
+            SecondPointCaptured = 4,
+
+            ThirdPointCaptured = 6,
+
+            FinalPointCaptured = 8
+        }
+
+        public AlphaStates AlphaState { get; set; }
+
+        public override void Update(Microsoft.Xna.Framework.GameTime gameTime)
+        {
+            base.Update(gameTime);
+
+            this.Alpha =  ((float)AlphaState / 10f);
+        }
+
+        public void NextState()
+        {
+            if (AlphaState != AlphaStates.FinalPointCaptured)
+            {
+                AlphaState += 2;
+            }
+        }
+    }
+}
