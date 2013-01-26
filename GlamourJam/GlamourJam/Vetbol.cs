2d5ec34a1c3bd8ee4284f634c8c3e8b0bf37fa68
Gunnaway <stevengunneweg@gmail.com>
2013-01-26 15:45:06 +0100

    geen slide meer na jump off wall... verder weet ik het niet meer

--- GlamourJam/GlamourJam/Vetbol.cs
+++ GlamourJam/GlamourJam/Vetbol.cs
@@ -20,18 +20,18 @@ namespace GlamourJam
 		public GamePadState padState;
 		public GamePadState prevPadState;
 		public int maxSpeed = 350;
-        public int jumpSpeed = 500;
-        public int extraJump = 50;
-        public float speedX = 0;
-        public float speedY = 0;
+		public int jumpSpeed = 500;
+		public int extraJump = 50;
+		public float speedX = 0;
+		public float speedY = 0;
 		public Vector2 jumpDirection = new Vector2(0, -1);
-        public bool capturing = false;
-        public PlayerIndex index;
-        public string CollisionState = "idle";
+		public bool capturing = false;
+		public PlayerIndex index;
+		public string CollisionState = "idle";
 
 		public Vetbol(PlayerIndex playerIndex)
 		{
-            index = playerIndex;
+			index = playerIndex;
 			Position = new Vector2(100, 100);
 			image.LoadTexture(Controller.Content.Load<Texture2D>("images/slimeblob"), 48, 48);
 			image.AddAnimation("IDLE", new int[1] { 0 }, 0);
@@ -52,8 +52,8 @@ namespace GlamourJam
 		public override void Update(GameTime gameTime)
 		{
 			base.Update(gameTime);
-            Controller.Collide(this, "tilemap", Collision);
-            Controller.Collide(this, "capturePoint", null, BeingCaptured);
+			Controller.Collide(this, "tilemap", Collision);
+			Controller.Collide(this, "capturePoint", null, BeingCaptured);
 			padState = GamePad.GetState(index);
 
 			if (speedX < 0)
@@ -65,157 +65,95 @@ namespace GlamourJam
 			}
 
 			//Move when sticking
-            speedY += 15;
-            if(CollisionState == "idle"){
-            //Move Horizontally
-			if (!(padState.ThumbSticks.Left.X == 0) && !isSticking)
-            {
-                if (onfloor)
-                {
-                    if ((-1 * maxSpeed) <= speedX && speedX <= (1 * maxSpeed))
-                    {
-                        speedX = padState.ThumbSticks.Left.X * maxSpeed;
-                    }
-                 }else{
-                     if ((-1 * maxSpeed) <= speedX && speedX <= (1 * maxSpeed))
-                     {
-                         speedX = padState.ThumbSticks.Left.X * (maxSpeed);
-                     }
-                }
-			} else
-            {
-                if (onfloor)
-                {
-                    speedX *= 0.90f;
-                }
-                else
-                {
-                    speedX *= 0.99f;
-                }
+			speedY += 15;
+			if (CollisionState == "idle")
+			{
+				//Move Horizontally
+				if (!(padState.ThumbSticks.Left.X == 0) && !isSticking)
+				{
+					if (onfloor)
+					{
+						if ((-1 * maxSpeed) <= speedX && speedX <= (1 * maxSpeed))
+						{
+							speedX = padState.ThumbSticks.Left.X * maxSpeed;
+						}
+					} else
+					{
+						if ((-1 * maxSpeed) <= speedX && speedX <= (1 * maxSpeed))
+						{
+							speedX = padState.ThumbSticks.Left.X * (maxSpeed);
+						}
+					}
+				} else
+				{
+					if (onfloor)
+					{
+						speedX *= 0.90f;
+					} else
+					{
+						speedX *= 0.99f;
+					}
+				}
+
+				if (onfloor)
+				{
+					if (speedY >= 5)
+					{
+						//Jump
+						if (padState.Buttons.A == ButtonState.Pressed && prevPadState.Buttons.A != ButtonState.Pressed)
+						{
+							//Jump();
+							onfloor = false;
+							speedY = -jumpSpeed;
+						}
+					}
+				}
+			}
+			if (CollisionState == "bottom")
+			{
 			}
 
-                if (onfloor)
-                {
-                    if(speedY>=5){
-                        //Jump
-                        if (padState.Buttons.A == ButtonState.Pressed && prevPadState.Buttons.A != ButtonState.Pressed)
-                        {
-                            //Jump();
-                            onfloor = false;
-                            speedY = -jumpSpeed;
-                        }
-                    }
-                }
-            }
-            if (CollisionState=="bottom")
-            {
-               /* speedY = 0;
-                //Move Horizontally
-                if (!(padState.ThumbSticks.Left.X == 0) && !isSticking)
-                {
-                    speedX = padState.ThumbSticks.Left.X * maxSpeed;
-                }
-                else
-                {
-                    speedX = 0;
-                }
-                
-              
-                if (!onfloor)
-                {
-                   // CollisionState = "idle";
-                }
-                else
-                {
-                    speedY = 0;
-                }
-                //Jump
-                if (padState.Buttons.A == ButtonState.Pressed && prevPadState.Buttons.A != ButtonState.Pressed)
-                {
-                    //Jump();
-                    speedY = -jumpSpeed;
-                    CollisionState = "idle";
-                }*/
-            }
-            
-            if(CollisionState == "left"){
-                //Move Horizontally
-              /* if (!(padState.ThumbSticks.Left.X == 0) && !isSticking)
-                {
-                    if ((-1 * maxSpeed) <= speedX && speedX <= (1 * maxSpeed))
-                    {
-                        speedX = padState.ThumbSticks.Left.X * maxSpeed;
-                    }
-                }
-                  else
-               {
-                  speedX = 0;
-               }*/
-                speedY = 0f;
-                //Jump
-                if (padState.Buttons.A == ButtonState.Pressed && prevPadState.Buttons.A != ButtonState.Pressed)
-                {
-                    //Jump();
-                    speedX += jumpSpeed + extraJump;
-                    speedY = -jumpSpeed/2;
-                    CollisionState = "idle";
-                }
-                if ((padState.ThumbSticks.Left.X > 0))
-                {
-                  //  CollisionState = "idle";
-                }
-            }
-             if(CollisionState == "right"){
-                 //Move Horizontally
-               /* if (!(padState.ThumbSticks.Left.X == 0) && !isSticking)
-                {
-                     if ((-1 * maxSpeed) <= speedX && speedX <= (1 * maxSpeed))
-                     {
-                         speedX = padState.ThumbSticks.Left.X * maxSpeed;
-                     }
-                }
-                   else
-                {
-                   speedX = 0;
-                }*/
-                 speedY *= 0f;
-                 //Jump
-                 if (padState.Buttons.A == ButtonState.Pressed && prevPadState.Buttons.A != ButtonState.Pressed)
-                 {
-                     //Jump();
-                     speedX -= jumpSpeed + extraJump;
-                     speedY = -jumpSpeed/2;
-                     CollisionState = "idle";
-                 }
-                 if((padState.ThumbSticks.Left.X < 0)){
-                    // CollisionState = "idle";
-                 }
-            }
-            if(CollisionState == "top"){
-                 speedY += 5;
-            }
-            if (isSticking)
-                /*{
-                    speedX = padState.ThumbSticks.Left.X * (maxSpeed / 2);
-                    speedY = -padState.ThumbSticks.Left.Y * (maxSpeed / 2);
-                } else
-                //Gravity
-                {
-                    speedY += 5;
-                }*/
-
-                speedY *= 0.95f;
-            Velocity.X = speedX;
-            Velocity.Y = speedY;
-
-           // System.Diagnostics.Debug.WriteLine("[VetBol]State:["+CollisionState+"]V.Y:[" + Velocity.Y + "]V.X[" + Velocity.X + "]");
-			//Jump
-			//if (padState.Buttons.A == ButtonState.Pressed && prevPadState.Buttons.A != ButtonState.Pressed && onfloor)
-			//{
-			//	onfloor = false;
-				//Jump();
-			//	Velocity.Y = -jumpSpeed;
-			//}
+			if (CollisionState == "left")
+			{
+				//Move Horizontally
+				speedY = 0f;
+				//Jump
+				if (padState.Buttons.A == ButtonState.Pressed && prevPadState.Buttons.A != ButtonState.Pressed)
+				{
+					//Jump();
+					speedX += jumpSpeed + extraJump;
+					speedY = -jumpSpeed;
+					CollisionState = "idle";
+				}
+				if ((padState.ThumbSticks.Left.X > 0))
+				{
+					//  CollisionState = "idle";
+				}
+			}
+			if (CollisionState == "right")
+			{
+				speedY *= 0f;
+				//Jump
+				if (padState.Buttons.A == ButtonState.Pressed && prevPadState.Buttons.A != ButtonState.Pressed)
+				{
+					//Jump();
+					speedX -= jumpSpeed + extraJump;
+					speedY = -jumpSpeed;
+					CollisionState = "idle";
+				}
+				if ((padState.ThumbSticks.Left.X < 0))
+				{
+					// CollisionState = "idle";
+				}
+			}
+			if (CollisionState == "top")
+			{
+				speedY += 5;
+			}
+			if (isSticking)
+				speedY *= 0.95f;
+			Velocity.X = speedX;
+			Velocity.Y = speedY;
 
 
 			//ANIMATIONS
@@ -259,74 +197,46 @@ namespace GlamourJam
 						image.Rotation = -(float)Math.Atan2(Velocity.X, Velocity.Y) - MathHelper.ToRadians(-225);
 						break;
 				}
-
 			}
 
 			//RESET FOR NEXT FRAME
 			isSticking = false;
 			prevPadState = padState;
 		}
-        public bool BeingCaptured(Node player, Node capturePoint)
-        {
-            if (player.Touching.Bottom)
-            {
-                (capturePoint as CapturePoint).startCapturing(this);
-            }
+		public bool BeingCaptured(Node player, Node capturePoint)
+		{
+			if (player.Touching.Bottom)
+			{
+				(capturePoint as CapturePoint).startCapturing(this);
+			}
 
-            return false;
-        }
+			return false;
+		}
 		public void Collision(Node player, Node collidingTile)
-        {
-            /*jumpDirection = Vector2.Zero;
+		{
+			if (player.Touching.Bottom)
+			{
+				onfloor = true;
+				speedY = 0;
+				speedX = 0;
+				// CollisionState = "bottom";
+			}
 
-            if (player.Touching.Bottom)
-            {
-                jumpDirection.Y = -1;
-            }
-            if (player.Touching.Left)
-            {
-                jumpDirection.X = 1;
-            }
-            if (player.Touching.Right)
-            {
-                jumpDirection.X = -1;
-            }
-            if (player.Touching.Top)
-            {
-                jumpDirection.Y = 1;
-            }*/
-            if (player.Touching.Bottom)
-            {
-                onfloor = true;
-                speedY = 0;
-               // CollisionState = "bottom";
-            }
-            
-            if (player.Touching.Left){
-                CollisionState = "left";
-                speedX = 0;
-            }
-            if (player.Touching.Right)
-            {
-                CollisionState = "right";
-                speedX = 0;
-            }
-            if (player.Touching.Top)
-            {
-                speedY = 0;
-            }
-
-            //System.Diagnostics.Debug.WriteLine("[VetBol]CollisionState:" + CollisionState);
-            //if (player.Touching.Bottom || player.Touching.Left || player.Touching.Right)
-            //{
-            //	onfloor = true;
-            //}
-            //if (player.Touching.Left || player.Touching.Right || player.Touching.Top)
-            //{
-            //	isSticking = true;
-            //	onfloor = true;
-            //}
-        }
+			if (player.Touching.Left)
+			{
+				CollisionState = "left";
+				speedX = 0;
+			}
+			if (player.Touching.Right)
+			{
+				CollisionState = "right";
+				speedX = 0;
+			}
+			if (player.Touching.Top)
+			{
+				speedY = 0;
+			}
+		}
 
 		public void Jump()
 		{
